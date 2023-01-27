3ea1c97abfab339962ca9972fd6a8c3ef88f5daa
SivaKrishna <shivataraya@gmail.com>
2023-01-27 22:34:31 +0530

    scoped thread for auto join

---
+++ src/bin/c02e02-progress-reporting.rs
@@ -0,0 +1,34 @@
+use std::sync::atomic::AtomicUsize;
+use std::sync::atomic::Ordering;
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    let num_done = AtomicUsize::new(0);
+
+    thread::scope(|s| {
+        // A background thread to process all 100 items.
+        s.spawn(|| {
+            for i in 0..100 {
+                process_item(i); // Assuming this takes some time.
+                num_done.store(i + 1, Ordering::Relaxed);
+            }
+        });
+
+        // The main thread shows status updates, every second.
+        loop {
+            let n = num_done.load(Ordering::Relaxed);
+            if n == 100 {
+                break;
+            }
+            println!("Working.. {n}/100 done");
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    println!("Done!");
+}
+
+fn process_item(_: usize) {
+    thread::sleep(Duration::from_millis(37));
+}
