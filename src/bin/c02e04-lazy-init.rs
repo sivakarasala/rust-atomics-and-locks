6764cfeb3d76063bd36e445ecec676afa66ea57a
SivaKrishna <shivataraya@gmail.com>
2023-01-29 21:24:56 +0530

    lazy init

---
+++ src/bin/c02e04-lazy-init.rs
@@ -0,0 +1,23 @@
+use std::sync::atomic::Ordering::Relaxed;
+use std::thread;
+use std::{sync::atomic::AtomicU64, time::Duration};
+
+fn get_x() -> u64 {
+    static X: AtomicU64 = AtomicU64::new(0);
+    let mut x = X.load(Relaxed);
+    if x == 0 {
+        x = calculate_x();
+        X.store(x, Relaxed);
+    }
+    x
+}
+
+fn calculate_x() -> u64 {
+    thread::sleep(Duration::from_secs(1));
+    123
+}
+
+fn main() {
+    dbg!(get_x());
+    dbg!(get_x());
+}
